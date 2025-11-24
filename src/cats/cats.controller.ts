1b2d632021b840155822e22efcb1a4dd64796837
ije90s <idgocm@gmail.com>
2025-11-24 16:57:20 +0900

    예외필터

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseFilters, ForbiddenException } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { HttpExceptionFilter } from 'src/common/http-exception.filter';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Post()
+  //@UseFilters(new HttpExceptionFilter)
   create(@Body() createCatDto: CreateCatDto) {
+    //throw new ForbiddenException();
     return this.catsService.create(createCatDto);
   }
 
